6a44e21a5c097679a4b4f5049794b1f551863691
George <salixs@mail.ru>
2019-08-11 19:25:58 +0300

    123

---
+++ www/index.php
@@ -0,0 +1,348 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Asap:500" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <title>OENRIQUE</title>
+  </head>
+  <body class="container-fluid">
+    
+      <header class="headbg clearfix">
+        <div class="container-fluid">
+          <nav class="headnavbar">
+            <div id="scrill" class="logo1"><img src="img/2.png"></div>
+              <ul class="navigation1">
+              <div class="pading12"> <li><a href="">Home</a></li> </div>
+              <div class="pading12"> <li><a href="">About</a></li> </div>
+              <div class="pading12"><li><a href="">Service</a></li> </div>
+              <div class="pading12"><li><a href="">Portfolio</a></li></div> 
+              <div ><li><a href="">Contact</a></li> </div>
+              </ul>
+          </nav>
+
+      
+      <aside class="asidehead">
+        
+            <span class="number1">Luis Oenrique <p class="number2"> User Experience / User Interface Expert</p></span>
+            <div class="buttondiv"><button class="btn btn-light button1">KNOW MORE</button><button class="btn btn-outline-dark button2">HIRE ME</button></div>
+            <a class="navigati1e" href="#hre12"><img src="img/12.png"></a>
+      </aside>
+    </div>
+      </header>
+       
+      <section class="mainsec">
+        <div class="container-fluid">
+         <div class="row">
+            <div class="col-xs-6 uui12">
+            
+        <img src="img/3.png">
+       <p class="number3">KNOW MORE<br>
+ABOUT ME</p>
+        <div style="max-width: 100%">
+        <p class="number4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean<br> commodo ligula eget dolor. Aenean massa. Cum sociis natoque<br> penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br> Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.<br> Nulla consequat massa quis enim.</p>
+          </div>
+        <div id="hre12" class="divtest"><button class="btn btn-outline-dark button5">DOWNLOAD MY CV</button>
+        </div>
+    
+       </div>
+       <div class="col-xs-6 uuu21" > 
+       <center>  <img src="img/4.png"></center>
+       </div>
+     </div>
+   </div>
+      </section>
+
+      <section class="nextonesectopn">
+         <div class="container-fluid">
+          <center><img src="img/5.png"></center>
+          <p class="sercidw">sERVICES</p>
+          <div class="row oooqi1">
+            <div class="col-xs-4 yyy2">
+              <center><img src="img/6.png"></center>
+              <p class="maintext1">User Research</p>
+              
+            </div> 
+             <div class="col-xs-4 yyy2">
+              <center><img src="img/7.png"></center>
+               <p class="maintext1" style="margin-top: 10px">IA And Wireframes</p>
+            </div> 
+             <div class="col-xs-4 yyy2">
+              <center><img src="img/8.png"></center>
+               <p class="maintext1">User Testing</p>
+            </div> 
+        </div>
+
+
+         <div class="row oooqi1">
+            <div class="col-xs-4 yyy2">
+              <center><img src="img/9.png"></center>
+               <p class="maintext1" style="line-height: 30px; margin-top:20px;">Looks and Feel<br>
+Design (UI)</p>
+              <p ></p>
+              
+            </div> 
+             <div class="col-xs-4 yyy2">
+              <center><img src="img/10.png"></center>
+               <p class="maintext1" style="margin-top: 10px">Delivery same12</p>
+              
+            </div> 
+             <div class="col-xs-4 yyy2">
+              <center><img src="img/11.png"></center>
+               <p class="maintext1" >Integration </p>
+            </div>
+            
+          </div>
+
+      </section>
+
+
+      <section class="newsection">
+        <div class="container-fluid uu12341">
+           <center><img src="img/5.png"></center>
+          <p class="sercidw">MY SKILLS</p>
+          <div class="uui1xkk">
+          <center><img src="img/13.png"></center>
+        </div>
+        </div>
+      </section>
+
+
+      <section class="portfolio123">
+      <div class="container-fluid">
+           <center><img src="img/5.png"></center>
+          <p class="sercidw">PORTFOLIO</p>
+      <div style="margin:150px auto">
+        <div class="row">
+          <div class="col-xs-4 iii12">
+          <li><span class="texttransit">Any text</span> <center> <img src="img/14.png"></center></li>
+          </div>
+           <div class="col-xs-4 iii12">
+          <li><span class="texttransit">Any text</span> <center> <img src="img/15.png"></center></li>
+          </div>
+           <div class="col-xs-4 iii12">
+            <li><span class="texttransit">Any text</span> <center> <img src="img/16.png"></center></li>
+          </div>
+        </div>
+         <div class="row" style=" margin-top:10px">
+          <div class="col-xs-4 iii12">
+           <center> <img src="img/17.png"></center>
+          </div>
+           <div class="col-xs-4 iii12">
+           <center> <img src="img/18.png"></center>
+          </div>
+           <div class="col-xs-4 iii123">
+            <center><img src="img/19.png" width="369px" height="318px"></center>
+          </div>
+        </div>
+      
+</div>
+
+
+  <div id="acordion" role="tablist" aria-multiselectable="true">
+      <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="turbo">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#acordion" href="#uuqo12" aria-expected="true" aria-controls="uuqo12"><center><button class="btn btn-light qwerq1">LOAD MORE</button></center></a>
+            </h4>
+          </div>
+          <div class="panel-collapse collapse in" id="uuqo12" role="tabpanel" aria-labelledby="turbo">
+            <div class="container-fluid">
+             <div class="row" style="margin-top:-150px">
+          <div class="col-xs-4 iii12">
+           <center> <img src="img/14.png"></center>
+          </div>
+           <div class="col-xs-4 iii12">
+           <center> <img src="img/15.png"></center>
+          </div>
+           <div class="col-xs-4 iii12">
+            <center><img src="img/16.png"></center>
+          </div>
+        </div>
+         <div class="row" style=" margin-top:10px">
+          <div class="col-xs-4 iii12">
+           <center> <img src="img/17.png"></center>
+          </div>
+           <div class="col-xs-4 iii12">
+           <center> <img src="img/18.png"></center>
+          </div>
+           <div class="col-xs-4 iii123">
+            <center><img src="img/19.png" width="369px" height="318px"></center>
+          </div>
+        </div>
+            <center><button  class="btn btn-light qwerq2">LOAD MORE</button></center>
+          </div>
+        </div>
+      </div>
+
+
+  </div>
+
+      </div>
+      </section>
+
+      <section class="slidesection">
+        <div class="container-fluid">
+           <center><img src="img/5.png"></center>
+          <p class="sercidw45">TESTIMONIALS</p>
+                <div style="width: 1000px; height: auto; max-width: 100%; margin:150px auto; border:1px solid black">
+          <div class="slickfrst" style="width:790px; margin:80px auto; max-width: 100%;">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-xs-3">
+                  <img src="img/20.png">
+                </div>
+                <div class="col-xs-8" style=" width: 600px; margin:0 auto; max-width: 100%;">
+                  <p class="textmain">Limara tusi<br> <span class="fataleror"> CEO, RanDOM COMPANY</span></p>
+                 
+                  <p class="textext" style="text-transform: uppercase;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean<br> commodo ligula eget dolor. Aenean massa. Cum sociis natoque<br> penatibus et magnis dis parturient montes, nascetur ridiculus mus.<br> Donec quam felis, ultricies nec,</p>
+                </div>
+                
+              </div>
+
+            </div>
+            <div>qwewqeqw</div>
+            <div>qwewqeq</div>
+          </div>
+        </div>
+        </div>
+      </section>
+     
+
+      <section class="width124"> 
+        <div class="container-fluid">
+        <div class="row">
+          <div class="col-xs-3 pppq21">
+            <center><img src="img/23.png"></center>
+            <p style="font-size: 25px; text-align: center;  margin-top:20px; font-family: 'Asap-Bold'; "> 120<br> <span style="font-size:20px;font-family: 'Asap', sans-serif; text-transform: uppercase;"> Hands to Work </span></p>
+          </div>
+          <div class="col-xs-3 pppq22">
+            <center><img src="img/24.png"></center>
+             <p style="font-size: 25px; text-align: center;  margin-top:20px; font-family: 'Asap-Bold'; "> 310<br> <span style="font-size:20px;font-family: 'Asap', sans-serif; text-transform: uppercase;"> Happy client </span></p>
+          </div>
+          <div class="col-xs-3 pppq22">
+            <center><img src="img/25.png"></center>
+             <p style="font-size: 25px; text-align: center;  margin-top:20px; font-family: 'Asap-Bold'; "> 510<br> <span style="font-size:20px;font-family: 'Asap', sans-serif; text-transform: uppercase;"> finished project </span></p>
+          </div>
+          <div class="col-xs-3 pppq23">
+           <center><img src="img/26.png"></center>
+             <p style="font-size: 25px; text-align: center;  margin-top:20px; font-family: 'Asap-Bold'; "> 720<br> <span style="font-size:20px;font-family: 'Asap', sans-serif; text-transform: uppercase;"> cups of coffe</span></p>
+          </div>
+        </div> 
+          
+        </div>
+      </section>
+
+
+
+      <footer class="endthisshit">
+        <div class="container-fluid">
+          <center style="padding-top:20px;"><img src="img/28.png"></center>
+          <p class="sercidw46">SAY HELLO!</p>
+          <form class="form">
+            <div style="width:775px; max-width: 100%; margin-left:auto; margin-right: auto; margin-top:70px;">
+              <div class="row">
+                <div class="col-xs-6 iiiooop1">
+            <input class="form-control qwrdsa" type="text" name="Name" placeholder="Name" required></div>
+            <div class="col-xs-6 iiiooop1">
+            <input class="form-control qwrdsa1" type="email" name="mail" placeholder="Email" required>
+          </div>
+          </div>
+          <textarea  class="form-control qwrdsa3" type="text" name="aria" placeholder="Message" required></textarea>
+         <center> <button class="btn btn-light button8">Send <img src="img/29.png"></button></center>
+              </div>
+
+
+          </form>
+          <a class="knopka" href="#scrill"><img  src="img/30.png"></a>
+          <div class="tair"> 
+         <center> <img src="img/31.png"></center>
+       </div>
+   
+       <div style="width: 1175px; max-width: 100%; margin:0 auto">
+         <div style="width: 190px; margin-top:10px; max-width: 100%; float:left">
+              <div class="container-fluid">
+          <div class="row">
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/32.png"></center>
+            </div>
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/33.png"></center>
+            </div>
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/34.png"></center>
+            </div>
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/35.png"></center>
+            </div>
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/36.png"></center>
+            </div>
+            <div class="col-xs-2 " style="margin:0 auto">
+              <center><img src="img/37.png"></center>
+            </div>
+
+            </div>
+           
+          </div>
+         </div>
+         <div style="width: 600px; max-width: 100%;margin:10px auto">
+         <center><p class="lastofus">2017. All Right Reserved</p></center>
+       </div>
+       </div>
+     
+        </div>
+      </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+    <script src="js/slick.js"></script>
+    <script type="text/javascript">
+
+      $('.navigati1e').click(function(){
+           $('html,body').animate({scrollTop:800},1000);
+
+
+      });
+      $('.qwerq1').click(function(){
+        $('.qwerq1').hide();
+
+      });
+       $('.qwerq1').click(function(){
+        $('.qwerq1').hide();
+         $('.qwerq1').css('transition','all .9s ease')
+
+      });
+       $('.slickfrst').slick({
+        dots:true
+       });
+
+       $('.knopka').click(function(){
+        $('html,body').animate({scrollTop:0},2000)
+       });
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
